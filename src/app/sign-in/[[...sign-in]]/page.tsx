9ee5fbbf880def051fe9849af920deb070ebbe9c
PRAKALP-PANDE <prakalpsp16@gmail.com>
2025-07-23 00:44:43 +0530

    ENH: Auth

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,9 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-    return <div className='h-[calc(100vh-96px)] flex items-center justify-center'>
-        <SignIn />
-    </div>
+    return (
+        <div className='h-[calc(100vh-96px)] flex items-center justify-center'>
+            <SignIn />
+        </div>
+    )
 }
