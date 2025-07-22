4aa112cf49121f2530ccb7770d5e62793a3904a4
PRAKALP-PANDE <prakalpsp16@gmail.com>
2025-07-23 00:53:01 +0530

    ENH: Search bar and navbar items alignment

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
                 <Link href='/' className="font-bold text-xl text-blue-600" >S O C I A L</Link>
             </div>
             {/* Center */}
-            <div className="hidden md:flex w-[50%] text-sm">
+            <div className="hidden md:flex w-[50%] text-sm items-center justify-between">
                 {/* Links  */}
                 <div className="flex gap-6 text-gray-600">
                     <Link href="/" className="flex items-center gap-2">
@@ -27,6 +27,10 @@ const Navbar = () => {
                         <span>Stories</span>
                     </Link>
                 </div>
+                <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+                    <input type="text" placeholder="search..." className="bg-transparent outline-none" />
+                    <Image src="/search.png" alt="" width={14} height={14} />
+                </div>
             </div>
             {/* Right  */}
             <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
@@ -37,7 +41,7 @@ const Navbar = () => {
                 <ClerkLoaded>
                     <SignedIn>
                         <div className="cursor-pointer">
-                            <Image src="/people.png" alt="" width={20} height={20} />
+                            <Image src="/people.png" alt="" width={24} height={24} />
                         </div>
                         <div className="cursor-pointer">
                             <Image src="/messages.png" alt="" width={20} height={20} />
